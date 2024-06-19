1f01dba9195d26fb1cfe6525476f18386ac1f6dd
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2024-06-19 17:28:38 -0300

    initialie test

--- programs/potrero-olimpcs/src/lib.rs
+++ programs/potrero-olimpcs/src/lib.rs
@@ -1,37 +1,67 @@
 use anchor_lang::prelude::*;
 
-declare_id!("BAewG8BBJnVbxwNfDepJUCmqJMbMwm1TnabBWnbmWK6X");
+declare_id!("5bJEJvsNtYXWMyqecqV8dr8Tdip6pqPbM1iaNVbUr3iK");
 
+const PROJECT_TREASURY: &str = "GtrjYbtvJ9T5oP1P64gY2yBLXcDtKERgNp5o1k6ty7Mj";
 #[program]
 pub mod potrero_olimpcs {
 
+    use std::str::FromStr;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, name: String, resolultion_time: i64) -> Result<()> {
-        ctx.accounts.oracle_event.set_inner(
-            OracleEvent {
-                resolultion_time,
-                resolver: *ctx.accounts.resolver.to_account_info().key,
-                bump: ctx.bumps.oracle_event,
-                name
-            }
-        );
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        name: String,
+        resolultion_time: i64,
+        open_until_time: i64,
+    ) -> Result<()> {
+        ctx.accounts.oracle_event.set_inner(OracleEvent {
+            resolultion_time,
+            open_until_time,
+            resolver: *ctx.accounts.resolver.to_account_info().key,
+            bump: ctx.bumps.oracle_event,
+            name,
+        });
         Ok(())
     }
 
-    pub fn make_prediction(ctx: Context<MakePrediction>, _name: String, gold: u8, silver: u8, bronze: u8)-> Result<()> {
-        let clock = Clock::get()?;
-        ctx.accounts.prediction.set_inner(
-            PodiumPrediction {
-                gold,
-                silver,
-                bronze,
-                event: *ctx.accounts.oracle_event.to_account_info().key,
-                timestamp:clock.unix_timestamp,
-                owner: ctx.accounts.player.key(),
-                bump: ctx.bumps.prediction
-            }
+    pub fn make_prediction(
+        ctx: Context<MakePrediction>,
+        _name: String,
+        gold: u8,
+        silver: u8,
+        bronze: u8,
+    ) -> Result<()> {
+        require_keys_eq!(
+            ctx.accounts.project_treasury.key(),
+            Pubkey::from_str(PROJECT_TREASURY).unwrap()
         );
+        let clock = Clock::get()?;
+        ctx.accounts.prediction.set_inner(PodiumPrediction {
+            gold,
+            silver,
+            bronze,
+            event: *ctx.accounts.oracle_event.to_account_info().key,
+            timestamp: clock.unix_timestamp,
+            owner: ctx.accounts.player.key(),
+            bump: ctx.bumps.prediction,
+        });
+
+        let transfer_instruction = anchor_lang::solana_program::system_instruction::transfer(
+            ctx.accounts.player.to_account_info().key,
+            ctx.accounts.project_treasury.to_account_info().key,
+            100,
+        );
+
+        let _ = anchor_lang::solana_program::program::invoke(
+            &transfer_instruction,
+            &[
+                ctx.accounts.player.to_account_info(),
+                ctx.accounts.project_treasury.to_account_info(),
+            ],
+        );
+
         Ok(())
     }
 }
@@ -46,15 +76,13 @@ pub struct Initialize<'info> {
         payer = maker,
         seeds = [b"OracleEvent".as_ref(), name.as_ref()],
         bump,
-        space = 40,
+        space = 100,
     )]
     pub oracle_event: Account<'info, OracleEvent>,
     /// CHECK: we just passing it to store the pubkey
     pub resolver: UncheckedAccount<'info>,
     pub system_program: Program<'info, System>,
-
 }
-
 
 #[derive(Accounts)]
 #[instruction(_name: String, seed: u64)]
@@ -69,11 +97,13 @@ pub struct MakePrediction<'info> {
     #[account(
         init,
         payer = player,
-    seeds = [b"PodiumPrediction".as_ref(),  _name.as_ref(), player.key().as_ref(), seed.to_le_bytes().as_ref()],
-    bump,
-    space = 40,
-)]
+        seeds = [b"PodiumPrediction".as_ref(),  _name.as_ref(), player.key().as_ref(), seed.to_le_bytes().as_ref()],
+        bump,
+        space =  8 + PodiumPrediction::INIT_SPACE,
+    )]
     pub prediction: Account<'info, PodiumPrediction>,
+    /// CHECK: This should match a constan Pubkey in the program
+    pub project_treasury: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
 }
 
@@ -91,8 +121,9 @@ pub struct PodiumPrediction {
 
 #[account]
 pub struct OracleEvent {
+    pub open_until_time: i64,
     pub resolultion_time: i64,
     pub resolver: Pubkey,
     pub bump: u8,
-    pub name: String
+    pub name: String,
 }
--- tests/potrero-olimpcs.ts
+++ tests/potrero-olimpcs.ts
@@ -1,16 +1,39 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { PotreroOlimpcs } from "../target/types/potrero_olimpcs";
+import { PublicKey, SystemProgram } from "@solana/web3.js"
+import * as web3 from "@solana/web3.js"
 
-describe("potrero-olimpcs", () => {
-  // Configure the client to use the local cluster.
+describe("olimpics", () => {
+  const name = "Test1"
+  const endTime = new anchor.BN(Date.now() + 1_000_000)
+  const open_until_time = new anchor.BN(Date.now() + 100_000)
+
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.PotreroOlimpcs as Program<PotreroOlimpcs>;
+  const maker = (program.provider as anchor.AnchorProvider).wallet;
+
+  const [oracleEvent] = web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("OracleEvent"), Buffer.from(name)],
+    program.programId,
+  )
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.initialize(
+      name,
+      endTime,
+      open_until_time
+    )
+      .accounts({
+        maker: maker.publicKey,
+        oracleEvent,
+        resolver: maker.publicKey
+      })
+      .rpc({skipPreflight: true}).catch((e)=>console.log(e));
     console.log("Your transaction signature", tx);
+    const games = await program.account.podiumPrediction.all()
+    console.log(games)
   });
 });
